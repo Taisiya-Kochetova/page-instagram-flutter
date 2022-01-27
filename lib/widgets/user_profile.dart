09a0d2a4d556bd5b6b2d4f33d74e0058e43212b8
Taisiya-Kochetova <git.kochetova@gmail.com>
2022-01-27 10:42:26 +0300

    first commit

---
+++ lib/widgets/user_profile.dart
@@ -0,0 +1,207 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class UserProfile extends StatelessWidget {
+  List<MenuRowData> firstMenuRow = [
+    MenuRowData(Icons.favorite, 'Избранное'),
+    MenuRowData(Icons.call, 'Недавние звонки'),
+    MenuRowData(Icons.computer, 'Устройства'),
+    MenuRowData(Icons.folder, 'Папка с чатами'),
+  ];
+
+  List<MenuRowData> secondMenuRow = [
+    MenuRowData(Icons.notifications, 'Уведомления и звуки'),
+    MenuRowData(Icons.privacy_tip, 'Конфидециальность'),
+    MenuRowData(Icons.date_range, 'Данные и память'),
+    MenuRowData(Icons.brush, 'Оформление'),
+    MenuRowData(Icons.language, 'Язык'),
+    MenuRowData(Icons.sticky_note_2, 'Стикеры'),
+  ];
+
+  List<MenuRowData> thridMenuRow = [
+    MenuRowData(Icons.lock_clock, 'Apple Watch'),
+  ];
+
+  List<MenuRowData> fourthMenuRow = [
+    MenuRowData(Icons.help, 'Помощь'),
+    MenuRowData(Icons.question_answer, 'Вопросы в Telegram'),
+  ];
+
+  UserProfile();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Center(
+          child: Text('Настройки'),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: ListView(
+          children: [
+            _UserInfo(),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: firstMenuRow),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: secondMenuRow),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: thridMenuRow),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: fourthMenuRow),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  final IconData icon;
+  final String text;
+
+  MenuRowData(this.icon, this.text);
+}
+
+class _MenuWidget extends StatelessWidget {
+  final List<MenuRowData> menuRow;
+  const _MenuWidget({
+    Key? key,
+    required this.menuRow,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: menuRow.map((data) => _MenuWidgetRow(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  final MenuRowData data;
+
+  const _MenuWidgetRow({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(data.icon),
+          SizedBox(width: 15),
+          Expanded(child: Text(data.text)),
+          Icon(Icons.chevron_right_sharp),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserInfo extends StatelessWidget {
+  const _UserInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.white,
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              _AvatarWidget(),
+              SizedBox(height: 30),
+              _UserNameWidget(),
+              SizedBox(height: 10),
+              _UserPhoneWidget(),
+              SizedBox(height: 10),
+              _UserNickNameWidget(),
+            ],
+          ),
+        ),
+        Positioned(child: Text('Изм.'))
+      ],
+    );
+  }
+}
+
+class _UserNickNameWidget extends StatelessWidget {
+  const _UserNickNameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '@JonFir',
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 17,
+      ),
+    );
+  }
+}
+
+class _UserPhoneWidget extends StatelessWidget {
+  const _UserPhoneWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '+7(123)555 33 45',
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 15,
+      ),
+    );
+  }
+}
+
+class _UserNameWidget extends StatelessWidget {
+  const _UserNameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Evgenii <JonFir> Elchev',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  const _AvatarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      child: Placeholder(),
+    );
+  }
+}
